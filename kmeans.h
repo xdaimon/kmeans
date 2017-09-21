a385cb09c6be816314f554bfee1b6aa6a28b6896
Bradley Bauer <gazingatstars.o7@gmail.com>
2017-09-21 19:23:49 -0400

    Implement kmeans loop and accuracy functions.

--- kmeans.h
+++ kmeans.h
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace Eigen;
 
-template<int K, int Dimension>
+template<int K, int Dimension, int Iterations>
 class K_Means {
 public:
 	K_Means(){};
@@ -33,14 +33,19 @@ public:
 	// Clusters data and uses that information to predict data classes
 	// returns number_true_classifications
 	int Test(Data& test_data) {
-		// init centroids
-		// init centroid_ids
-		return 0;
+		randomInitCentroids(test_data);
+		for (int iter = 0; iter < Iterations; ++iter) {
+			findIdsOfClosestCentroids(test_data);
+			computeCentroids(test_data);
+			std::cout << iter << std::endl;
+		}
+		return computeAccuracy(test_data);
 	}
 
 private:
 
 	Matrix<double, K, 1> centroid_ids;
+	Matrix<double, K, 1> cluster_identity;
 
 	void randomInitCentroids(Data& test_data) {
 		const int N = test_data.examples.rows();
@@ -78,4 +83,39 @@ private:
 			centroids.row(k) /= cluster_size;
 		}
 	}
+
+	void findClusterIdentity(Data& test_data) {
+		// What digit is most likely associated with each cluster?
+		// loop through the examples in cluster k
+		//    find the examples label l
+		//    add one to count_vectors(k, l)
+		// index of element in count vector who's value is maximum is the most
+		// likely digit that the label represents
+
+		const int N = test_data.examples.rows();
+		Matrix<int, K, K> count_vectors;
+		count_vectors.setZero();
+		for (int i = 0; i < N; ++i) {
+			count_vectors(centroid_ids(i), test_data.labels(i)) += 1;
+		}
+		for (int k = 0; k < K; ++k) {
+			int max_count = 0;
+			for (int i = 0; i < K; ++i) {
+				if (count_vectors(k, i) > max_count) {
+					cluster_identity(k) = i;
+					max_count = count_vectors(k, i);
+				}
+			}
+		}
+	}
+
+	double computeAccuracy(Data& test_data) {
+		const int N = test_data.examples.rows();
+		findClusterIdentity(test_data);
+		double accuracy = 0.;
+		for (int i = 0; i < N; ++i)
+			accuracy += cluster_identity(centroid_ids(i)) == test_data.labels(i) ? 1 : 0;
+		return accuracy / N;
+	}
+
 };
